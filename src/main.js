ff342807457a3d8fce20e0c22b95da4999c40f20
Muntaeyeon <monty420420@naver.com>
2023-06-11 13:44:24 +0900

    json2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 function loadItems(){
-    return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json=> console.log(json))
+    return fetch('src/data/data.json')  //fetch함수 이용해서 데이터 받아오기
+    .then(response => response.json()) //성공적으로 받아왔으면 json으로 변환
+    .then(json=> json.items);
 }
 
 
 loadItems()
 .then(items => {
+    console.log(items);
     // displayItems(items);
     // setEventListeners(items)
 })
