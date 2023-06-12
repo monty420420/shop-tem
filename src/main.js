78d0fa204a024a42eee13a75775dcbd296d47fad
Muntaeyeon <monty420420@naver.com>
2023-06-12 23:50:31 +0900

    js innerhtml

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-function loadItems(){
-    return fetch('src/data/data.json')  //fetch함수 이용해서 데이터 받아오기
-    .then(response => response.json()) //성공적으로 받아왔으면 json으로 변환
-    .then(json=> json.items);
-}
-
-
-loadItems()
-.then(items => {
-    console.log(items);
-    // displayItems(items);
-    // setEventListeners(items)
-})
-.catch(console.log);
+function loadItems() {
+    return fetch('src/data/data.json')  
+      .then(response => response.json())
+      .then(json => json.items);
+  }
+  
+  function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+  }
+  
+  function createHTMLString(item) {
+    return `
+      <li class="items_item">
+        <img src="${item.image}" alt="${item.type}" class="items_item_thumb">
+        <span class="items_item_explain">${item.gender}, ${item.size}</span>
+      </li>
+    `;
+  }
+  
+  // main
+  loadItems()
+    .then(items => {
+    //   console.log(items);
+      displayItems(items);
+    })
+    .catch(console.log);
