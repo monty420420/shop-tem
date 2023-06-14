5b94c300a25eb394ec4e923b016da7af47a53834
Muntaeyeon <monty420420@naver.com>
2023-06-14 17:51:34 +0900

    js-filter

--- src/main.js
+++ src/main.js
@@ -18,11 +18,36 @@ function loadItems() {
     `;
   }
   
+  // onButtonClick 함수는 버튼을 클릭할 때 호출. 클릭된 버튼의 data-key와 data-value 값을 가져옵니다. 이 함수에서는 items 배열을 필터링하여 일치하는 조건을 가진 항목만 남기고, displayItems 함수를 호출하여 필터링된 항목을 표시.
+  function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = event.target.dataset.key;
+    //console.log(key); 
+    const value = event.target.dataset.value; // 클릭된 버튼의 data-value 값 가져오기
+    //console.log(value); 
+    const target = event.target;
+    // console.log(target);
+    //console.log(items)
+    if (key == null || value == null) {
+      return;
+    }
+    // console.log(items);
+    const filterd = items.filter(item => item[key] === value);
+    // console.log(filterd);
+    displayItems(filterd);
+  }
+  
+  function setEventListeners(items) {
+    const buttons = document.querySelector('.btn');
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click', ()=> displayItems(items));
+    buttons.addEventListener('click', (event) => onButtonClick(event, items));
+  }
+  
   // main
   loadItems()
     .then(items => {
-      console.log(items);
       displayItems(items);
-      //setEventListeners(items);
+      setEventListeners(items);
     })
     .catch(console.log);
